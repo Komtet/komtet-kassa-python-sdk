746dd6ce3a5419e563ebcdac9f4eaee46a047723
keipaglows <dark_inko@mail.ru>
2019-01-18 09:28:21 +0300

    version up

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('CHANGELOG.rst', 'r') as changelog:
 
 setup(
     name='komtet_kassa_sdk',
-    version='0.7.0',
+    version='0.8.0',
     license='MIT',
     description='Python SDK for KOMTET Kassa',
     long_description=long_description,
