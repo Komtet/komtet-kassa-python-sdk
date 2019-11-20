acb6928b0e377c6368ee707a7f583852d6db947f
kgurjev <kosmini4@gmail.com>
2019-11-20 11:13:06 +0300

    Обновлен setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('CHANGELOG.rst', 'r') as changelog:
 
 setup(
     name='komtet_kassa_sdk',
-    version='1.8.0',
+    version='1.9.0',
     license='MIT',
     description='Python SDK for KOMTET Kassa',
     long_description=long_description,
