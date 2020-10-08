9b43703215ede9bfe7d77bbb9b85a23446556c4a
Alexandr Sorokin <asorokin.motmom@gmail.com>
2020-10-08 11:50:01 +0300

    add apply_correction_positions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('CHANGELOG.rst', 'r') as changelog:
 
 setup(
     name='komtet_kassa_sdk',
-    version='5.1.0',
+    version='5.2.0',
     license='MIT',
     description='Python SDK for KOMTET Kassa',
     long_description=long_description,
