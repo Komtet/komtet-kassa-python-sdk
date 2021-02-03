4d13b991bfe5a12e2e5f4e6f48b07fd6124e2e68
Юрий Никитин <yura.motmom@gmail.com>
2021-02-03 15:33:45 +0300

    release 5.3.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('CHANGELOG.rst', 'r') as changelog:
 
 setup(
     name='komtet_kassa_sdk',
-    version='5.3.1',
+    version='5.3.2',
     license='MIT',
     description='Python SDK for KOMTET Kassa',
     long_description=long_description,
