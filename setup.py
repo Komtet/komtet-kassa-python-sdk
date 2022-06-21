455adea43b508e41dbf5b525a5e11af2b800061d
NWss <anikitin.mm@gmail.com>
2022-06-21 14:12:59 +0300

    release 7.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('CHANGELOG.rst', 'r') as changelog:
 
 setup(
     name='komtet_kassa_sdk',
-    version='7.0.3',
+    version='7.0.4',
     license='MIT',
     description='Python SDK for KOMTET Kassa',
     long_description=long_description,
