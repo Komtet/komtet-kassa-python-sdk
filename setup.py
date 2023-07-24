aa2a871214b1df6666cc09a6a7f177044e914d38
Коновалов Александр <aleksandrwork.motmom@gmail.com>
2023-07-24 10:16:44 +0300

    Добавление версии

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='komtet_kassa_sdk',
-    version='7.1.2',
+    version='7.1.3',
     license='MIT',
     description='Python SDK for KOMTET Kassa',
 
